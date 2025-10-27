bf4505d0648e403759b22f0c60b9f6ceda62a22d
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-10-27 15:15:20 +0530

    Explored hoisting

---
+++ Hoisting/hoist.js
@@ -0,0 +1,10 @@
+console.log(a)
+console.log(b)
+console.log(c)
+
+var a = 10
+let b = 20
+var c = 30
+
+//All the variable declarations are hoisted on the top, but let and const needs to initialized to access them. 
+// Whereas var can be accessed even before initialization.

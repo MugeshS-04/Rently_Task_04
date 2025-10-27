9011afb700497fffd11979509cf18d9127e93e36
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-10-27 13:42:04 +0530

    Changed the order

--- Variable_Types/sample.js
+++ Variable_Types/sample.js
@@ -5,9 +5,9 @@ var num3 = 15;
 //Re-assigning values
 num1 = 20;  // No error, changes the value of num1
 
-num3 = 30;  // No error, changes the value of num3
-
 num2 = 25;  // Error: Assignment to constant variable.
+
+num3 = 30;  // No error, changes the value of num3
 
 console.log("num1:", num1); // Outputs: num1: 20
 console.log("num2:", num2); // Outputs: num2: 10

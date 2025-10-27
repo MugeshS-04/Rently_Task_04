fd4b47e08e7c9c0c2e16c64cd4fb11f0cf9f49d7
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-10-27 12:51:06 +0530

    Exploring variable types

---
+++ Variable_Types/sample.js
@@ -0,0 +1,14 @@
+let num1 = 5;
+const num2 = 10;
+var num3 = 15;
+
+//Re-assigning values
+num1 = 20;  // No error, changes the value of num1
+
+num3 = 30;  // No error, changes the value of num3
+
+num2 = 25;  // Error: Assignment to constant variable.
+
+console.log("num1:", num1); // Outputs: num1: 20
+console.log("num2:", num2); // Outputs: num2: 10
+console.log("num3:", num3); // Outputs: num3: 30

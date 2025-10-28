623c082c62c81115f7a19f01cc328e083e666e8a
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-10-28 12:38:52 +0530

    Created object and implemented basic operations

---
+++ Objects/object.js
@@ -0,0 +1,15 @@
+let student = {
+    name: "Mugesh S",
+    age: "21",
+    course: "BE - CSE"
+}
+
+console.log(student)
+
+student.grade = "A" // adding new value
+
+console.log(student)
+
+delete student.age // deleting age value
+
+console.log(student)

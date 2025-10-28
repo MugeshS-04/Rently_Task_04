e721f5f5619da1f1c3727d6a3ac47d605b9a89ed
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-10-28 15:00:39 +0530

    Implemented the static function

---
+++ Objects/static.js
@@ -0,0 +1,23 @@
+function emp(){
+    name1 = "Mugesh S",
+    age = "21",
+    position = "Software Engineer"
+}
+
+//Instance method
+emp.prototype.display = function(name1, age, position) {
+    this.name1 = name1;
+    this.age = age;
+    this.position = position;
+    console.log(`Name: ${this.name1}, Age: ${this.age}, Position: ${this.position}`);
+}
+
+//Static method
+emp.display = function() {
+    console.log(`Name: ${name1}, Age: ${age}, Position: ${position}`);
+}
+
+let emp1 = new emp();
+emp1.display("Mugesh S", 21, "Software Engineer");
+
+emp.display();

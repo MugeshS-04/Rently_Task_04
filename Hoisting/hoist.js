7ac40a1483e13f4556a30cd63fdb3f0d50588a23
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-11-13 10:27:07 +0530

    Changed the var to const

--- Hoisting/hoist.js
+++ Hoisting/hoist.js
@@ -4,7 +4,7 @@ console.log(c)
 
 var a = 10
 let b = 20
-var c = 30
+const c = 30
 
 //All the variable declarations are hoisted on the top, but let and const needs to initialized to access them. 
 // Whereas var can be accessed even before initialization.
